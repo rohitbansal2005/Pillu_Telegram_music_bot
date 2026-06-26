f48bda2725af96ba1a09cb6a7d9c94a0b5f532a9
rkbansal <rohitkumarbansal23@gmail.com>
2026-06-26 15:16:09 +0530

    fix: add youtube datacenter bypass

--- PalluBot/utils/youtube.py
+++ PalluBot/utils/youtube.py
@@ -10,6 +10,9 @@ def get_yt_info_sync(query: str) -> dict:
         'format': 'bestaudio/best',
         'noplaylist': True,
         'quiet': True,
+        'extractor_args': {'youtube': ['player_client=android']},
+        'nocheckcertificate': True,
+        'ignoreerrors': True,
     }
     
     # Bypass YouTube Bot Protection using cookies
